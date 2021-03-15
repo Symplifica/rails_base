25bea1dac41a300fa32b35941d45c46a98176d4a
Andres Murcia <andres@picap.co>
2021-03-14 20:26:08 -0500

    testing config host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
 
-  before_filter :allow_iframe_requests
+  before_action :allow_iframe_requests
   def allow_iframe_requests
     response.headers.delete('X-Frame-Options')
   end
