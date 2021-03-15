4914aae7114ab6b41d82e4d5fa843a49ad2df6af
Andres Murcia <andres@picap.co>
2021-03-15 02:21:16 -0500

    fixing redis problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,17 +4,17 @@ ops_area = Area.where(name: "Operaciones").first_or_create
 na_area = Area.where(name: "Area no especifica").first_or_create
 
 # status caso base
-Status.create(name: "Abierto", area: na_area, closed: false)
+Status.where(name: "Abierto", area: na_area, closed: false).first_or_create
 
 # Status para SAC
-Status.create(name: "Abierto", area: sac_area, closed: false)
-Status.create(name: "Llamar mas tarde", area: sac_area, closed: false)
-Status.create(name: "No Resuelto (Cerrado)", area: sac_area, closed: true)
-Status.create(name: "Resuelto (Cerrado) ", area: sac_area, closed: true)
+Status.where(name: "Abierto", area: sac_area, closed: false).first_or_create
+Status.where(name: "Llamar mas tarde", area: sac_area, closed: false).first_or_create
+Status.where(name: "No Resuelto (Cerrado)", area: sac_area, closed: true).first_or_create
+Status.where(name: "Resuelto (Cerrado) ", area: sac_area, closed: true).first_or_create
 
 # Status para Ventas
-Status.create(name: "Por llamar (primer contacto)", area: sales_area, closed: false)
-Status.create(name: "En Progreso", area: sales_area, closed: false)
-Status.create(name: "Llamar mas tarde", area: sales_area, closed: false)
-Status.create(name: "Completado", area: sales_area, closed: true)
-Status.create(name: "Perdido", area: sales_area, closed: true)
+Status.where(name: "Por llamar (primer contacto)", area: sales_area, closed: false).first_or_create
+Status.where(name: "En Progreso", area: sales_area, closed: false).first_or_create
+Status.where(name: "Llamar mas tarde", area: sales_area, closed: false).first_or_create
+Status.where(name: "Completado", area: sales_area, closed: true).first_or_create
+Status.where(name: "Perdido", area: sales_area, closed: true).first_or_create
