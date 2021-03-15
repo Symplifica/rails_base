930bf5b22cd8b625171671d547f3c7cee941bddb
Andres Murcia <andres@picap.co>
2021-03-14 20:00:58 -0500

    adding root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
+  root "tickets#index"
   resources :tickets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
