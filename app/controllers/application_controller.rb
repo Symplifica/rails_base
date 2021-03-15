3da9d28cabe048c1c2513c3397a31a3e5ece56a3
Andres Murcia <andres@picap.co>
2021-03-14 20:17:04 -0500

    testing config host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
 
+  before_filter :allow_iframe_requests
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
   root "tickets#index"
   resources :tickets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
