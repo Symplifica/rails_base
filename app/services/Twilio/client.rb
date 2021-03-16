477990ba5db7a4d019780338fec04f78bb5dee5a
Edixon Hernandez <edixon.hernandez@symplifica.com>
2021-03-16 12:01:54 -0500

    create module Twilio

--- app/services/Twilio/client.rb
+++ app/services/Twilio/client.rb
@@ -1,10 +1,15 @@
 module Twilio
   class Client
     def initialize
-      @client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+      @client = Twilio::REST::Client.new(Rails.application.credentials.twilio[:ACCOUNT_SID], Rails.application.credentials.twilio[:TWILIO_AUTH_TOKEN])
     end
 
-    def task(workspace: ENV['TWILIO_WORKSPACE'], task_id: 'WTd0a188ff3dec7f799486381810f88f67')
+    # Get info task
+    # @example
+    #   c = Twilio::Client
+    #   t = c.task(id)
+    #   p t.reservations.list[0].worker_name
+    def task(workspace: Rails.application.credentials.twilio[:TWILIO_WORKSPACE], task_id: 'WTd0a188ff3dec7f799486381810f88f67')
       @client.taskrouter.workspaces(workspace).tasks(task_id).fetch
     end
   end
